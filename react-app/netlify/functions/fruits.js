6094e9ec647f7e92aa4a4526e1f47e19bdda5560
pb-06 <hjozsef91@protonmail.com>
2026-01-22 11:09:00 +0100

    Change GET body

--- react-app/netlify/functions/fruits.js
+++ react-app/netlify/functions/fruits.js
@@ -10,13 +10,7 @@ export const handler = async (event, context) => {
     if (event.httpMethod == 'GET') {
         return {
             statusCode: 200,
-            body: JSON.stringify([
-                { id: 1, name: 'Apple', healthy: true },
-                { id: 2, name: 'Banana', healthy: true },
-                { id: 3, name: 'Cherry', healthy: true },
-                { id: 4, name: 'Date', healthy: false },
-                { id: 5, name: 'Elderberry', healthy: true },
-            ]),
+            body: JSON.stringify([fruits]),
         }
     }
 
